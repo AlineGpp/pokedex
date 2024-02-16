49233a9b59176a1ffd5028c05a6beb1a2bdbb1f0
Aline Goppinger <91920984+AlineGpp@users.noreply.github.com>
2024-02-16 19:23:03 -0300

    :sparkles: feat: iniciando card com mais detalhes do pokemon

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -37,11 +37,3 @@ pokeApi.getPokemons = (offset = 0,limit = 5) =>{
     .then((detailRequests) => Promise.all(detailRequests))
     .then((pokemonsDetails) => pokemonsDetails)
 }
-
-// Promise.all([
-//     fetch('https://pokeapi.co/api/v2/pokemon/1'),
-//     fetch('https://pokeapi.co/api/v2/pokemon/2'),
-//     fetch('https://pokeapi.co/api/v2/pokemon/3'),
-//     fetch('https://pokeapi.co/api/v2/pokemon/4'),
-//     fetch('https://pokeapi.co/api/v2/pokemon/5'),
-// ]).then((results) => {console.log(results)})
